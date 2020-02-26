790c1a33bba9e079268a25cbb782fbd6fed01c0c
brijsavla <bs375@njit.edu>
2020-02-26 00:31:31 -0500

    Create Project1P2.py

---
+++ Project1P2.py
@@ -0,0 +1,10 @@
+class Node:
+  def __init__(self, value):
+    self.left = None
+    self.right = None
+    self.value = value
+#First we recurse down the left tree, then we recurse down the right tree
+def inOrder(root):
+  inOrder(root.left)
+  inOrder(root.right)
+
