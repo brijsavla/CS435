17935aa339b6ff2c572aae1a0845986a2f7e7481
brijsavla <bs375@njit.edu>
2020-05-12 09:59:54 -0400

    Update BSTIter.py

--- BSTIter.py
+++ BSTIter.py
@@ -4,45 +4,45 @@ class Node:
     self.left = None
     self.right = None
     self.value = value
+class BST:
+  def insertIter(root, value):
+    node = Node(value)
+    #Starting with the root which is the current node in the tree
+    curr = root
+    #Parent will be following 'curr', basically the node on the level above curr
+    parent = None
 
-def insertIter(root, value):
-  node = Node(value)
-  #Starting with the root which is the current node in the tree
-  curr = root
-  #Parent will be following 'curr', basically the node on the level above curr
-  parent = None
-
-  #Jumping from node to node in the tree, either left or right depending if the value of the node is greater than or less than the current value. If greater, then jump to the right child, otherwise jump to the left.
-  while curr != None:
-    #Parent jumps to current node, making 'curr' the new parent node
-    parent = curr
-    if node.value > curr.value:
-      #Jump right
-      curr = curr.right
+    #Jumping from node to node in the tree, either left or right depending if the value of the node is greater than or less than the current value. If greater, then jump to the right child, otherwise jump to the left.
+    while curr != None:
+      #Parent jumps to current node, making 'curr' the new parent node
+      parent = curr
+      if node.value > curr.value:
+        #Jump right
+        curr = curr.right
+      else:
+        #Jump left
+        curr = curr.left
+    #Checking if the tree is empty, if it is then the node becomes the parent 
+    if parent == None:
+      parent = node
+    #If parent value > node value, then node gets assigned to parent and becomes left child
+    elif parent.value > node.value:
+      parent.left = node
+    #If parent value < node value, then node gets assigned to parent and becomes right child
     else:
-      #Jump left
+      parent.right = node
+    return parent
+
+  #Traversing down the left child of each left subtree until we hit a null
+  def findMinIter(root):
+    curr = root
+    while curr.left != None:
       curr = curr.left
-  #Checking if the tree is empty, if it is then the node becomes the parent 
-  if parent == None:
-    parent = node
-  #If parent value > node value, then node gets assigned to parent and becomes left child
-  elif parent.value > node.value:
-    parent.left = node
-  #If parent value < node value, then node gets assigned to parent and becomes right child
-  else:
-    parent.right = node
-  return parent
+    return curr
 
-#Traversing down the left child of each left subtree until we hit a null
-def findMinIter(root):
-  curr = root
-  while curr.left != None:
-    curr = curr.left
-  return curr
-
-#Traversing down the right child of each right subtree until we hit a null
-def findMaxIter(root):
-  curr = root
-  while curr.right != None:
-    curr = curr.right
-  return curr
+  #Traversing down the right child of each right subtree until we hit a null
+  def findMaxIter(root):
+    curr = root
+    while curr.right != None:
+      curr = curr.right
+    return curr
